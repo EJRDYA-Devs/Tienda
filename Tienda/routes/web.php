ca54887ddc70c51bd45f2f4380b45d3d98ac8b14
Brayan Orellana <brayan.orellana@cu.ucsg.edu.ec>
2022-02-20 15:36:32 -0500

    Form - Reports - routes
    Create forms.blade to insert transactions, make table to reports and configurate web route

--- Tienda/routes/web.php
+++ Tienda/routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Reporte de Todos los Productos
+Route::get('/ReporteProductos','App\Http\Controllers\TransaccionesController@ReporteProductos');
+
+//Reporte de Todas las transacciones
+Route::get('/ReporteTransacciones','App\Http\Controllers\TransaccionesController@ReporteTransacciones');
+
+
+//llama formulario  en Controller para Ingresar Transaccion
+Route::get('/newTransaccion/create','App\Http\Controllers\TransaccionesController@create');
+
+//LLama metodo post en TransaccionesController
+Route::post('/newTransaccion','App\Http\Controllers\TransaccionesController@store'); 
+
+//llama formulario para escoger la transaccion a eliminar
+Route::get('/transaccion/delete','App\Http\Controllers\TransaccionesController@Eliminar'); 
+//Elimina Transaccion
+Route::get('/transaccionEliminada/{id}','App\Http\Controllers\TransaccionesController@Destroy'); //Elimina
